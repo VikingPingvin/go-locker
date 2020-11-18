5d3101b4077a0cce4bd72c99ebac09fe0c0e2480
Daniel Schreiber <sch1458@gmail.com>
2020-11-18 18:10:40 +0100

    Added --file flag parsing and basic OS file stat with error logging

--- agent/agent.go
+++ agent/agent.go
@@ -5,12 +5,21 @@ import (
 	"encoding/binary"
 	"fmt"
 	"net"
+	"os"
 	"vikingPingvin/locker/server"
 	"vikingPingvin/locker/server/protobuf"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/rs/zerolog/log"
 )
+
+// FileInput String Flag for Cobra CMD input
+var FileInput string
+
+// InputData Populated at the start of the program
+type InputData struct {
+	FileInput string
+}
 
 type Agent interface {
 	Start() bool
@@ -49,8 +58,34 @@ func (a ArtifactAgent) Start() bool {
 	return true
 }
 
+func parseInput() *InputData {
+	if len(FileInput) == 0 {
+		log.Info().Msg("No input file was given.")
+	}
+	data := &InputData{
+		FileInput: FileInput,
+	}
+
+	log.Info().Msgf("Input structure: %v", data)
+
+	return data
+}
+
+func parseFile(filePath *string) (err error) {
+	if _, err := os.Stat(*filePath); os.IsNotExist(err) {
+		log.Error().Msgf("Parsing file Input error: %v", err)
+	}
+	return err
+}
+
 // ExecuteAgent : Entrypoint for Locker agent start
 func ExecuteAgent() {
+	// Handle input flags
+	inputData := parseInput()
+
+	parseFile(&inputData.FileInput)
+
+	// Start Agent
 	agent := &ArtifactAgent{Port: "27001"}
 	agent.Start()
 }
